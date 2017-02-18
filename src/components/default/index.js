9b6df588398bf175edb5e884cc9e0d1f51c9fe30
theory friction practice <jackson.brebnor@gmail.com>
2017-02-17 23:15:28 -0500

    moves react builtin

--- src/components/default/index.js
+++ src/components/default/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './default.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BuiltIn, } from './components';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <BuiltIn />,
   document.getElementById('root')
 );
