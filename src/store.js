fa0f8b882d528ce7a124106a9d6820e39bf257af
theory friction practice <jackson.brebnor@gmail.com>
2017-02-17 23:18:02 -0500

    creates store js

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore, } from 'redux';
+
+// import { reducer as rootR, } from './modules';
+
+const collapsed = (getState, action) => action.type;
+const log = createLogger({ collapsed, });
+
+// export default state => applyMiddleware(thunk, log)(createStore)(rootR, state);
