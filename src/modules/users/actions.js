535d10debedb26e4eee2e6d21d40ae8c157931e4
theory friction practice <jackson.brebnor@gmail.com>
2017-02-19 18:16:29 -0500

    firebase sigInanon

--- src/modules/users/actions.js
+++ src/modules/users/actions.js
@@ -1,4 +1,22 @@
 import { ADD_USER, REMOVE_USER, } from './constants';
+import { connRef, } from '../../utils/firebase';
+import * as firebase from 'firebase';
+
+connRef.on('value', (snapshot) => {
+  if (snapshot.val()) {
+    firebase.auth().signInAnonymously().catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      if (errorCode === 'auth/operation-not-allowed') {
+        alert('You must enable Anonymous auth in the Firebase Console.');
+      } else {
+        console.error(error);
+      }
+    }); console.log('user slogged on', snapshot.val());
+  }
+});
 
 const add = user => arr => arr.concat(user);
 const remove = user => arr => arr;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase';
 export const config = {
-  apiKey: process.env.C4REDUX_FIREBASE_KEY,
+  apiKey: process.env.REACT_APP_C4REDUX_FIREBASE_KEY,
   authDomain: 'conn4redux.firebaseapp.com',
   databaseURL: 'https://conn4redux.firebaseio.com',
   storageBucket: 'conn4redux.appspot.com',
   messagingSenderId: '353870376265',
 };
 
+console.log(process.env.REACT_APP_C4REDUX_FIREBASE_KEY);
 firebase.initializeApp(config);
 export const db = firebase.database();
-const connRef = db.ref('.info/connected');
+
+export const connRef = db.ref('.info/connected');
 
 connRef.on('value', (snapshot) => {
   if (snapshot.val()) {
