e4652c9f70baaa6d8a969d32a8c106725c3fda81
theory friction practice <jackson.brebnor@gmail.com>
2017-02-19 15:35:44 -0500

    beofre editing index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BuiltIn, } from './components';
-import { AppDB, } from './utils';
+
 import './index.css';
 
 ReactDOM.render(
