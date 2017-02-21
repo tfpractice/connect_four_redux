ced9f5b8346b9e5c7baf5ef1b9889c611b135ce8
theory friction practice <jackson.brebnor@gmail.com>
2017-02-21 12:11:42 -0500

    index cleanup

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ import { connHandler, onlineHandler, } from './handlers';
 import { setCurrent, } from './modules/auth/actions';
 import { addUser, } from './modules/users/actions';
 
-// import App from './container';
 import Routes from './routes';
 import './index.css';
 injectTapEventPlugin();
+
 const store = getStore();
 
 connHandler(store);
-
 onlineHandler(store);
 
 ReactDOM.render(
@@ -24,40 +23,3 @@ ReactDOM.render(
     <Routes />
   </Provider>, document.getElementById('root')
 );
-
-// connRef.on('value', (snapshot) => {
-//   if (snapshot.val()) {
-//     console.log('addue', addUser('3'));
-//
-//     // store.dispatch(addUser('3'));
-//     fireApp.auth().signInAnonymously()
-//       .then((u) => {
-//         console.log('auth', fireApp.auth().currentUser);
-//         console.log('anon', u);
-//         return setCurrent(u);
-//       }).then(store.dispatch)
-//
-//       .catch((error) => {
-//         // Handle Errors here.
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//
-//         if (errorCode === 'auth/operation-not-allowed') {
-//           alert('You must enable Anonymous auth in the Firebase Console.');
-//         } else {
-//           console.error(error);
-//         }
-//       });
-//   }
-// });
-
-// fireApp.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     console.log('before auth state chnaged', user);
-//     setCurrent(user);
-//   } else {
-//     setCurrent(null);
-//
-//     // No user is signed in.
-//   }
-// });
