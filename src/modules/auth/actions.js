c6bbeb2cfb33f45830b7857d6eba97bf83568aeb
theory friction practice <jackson.brebnor@gmail.com>
2017-02-20 19:57:42 -0500

    c4 player

--- src/modules/auth/actions.js
+++ src/modules/auth/actions.js
@@ -1,9 +1,10 @@
 import { addOnline, addUser, } from '../users/actions';
 import { fireUtils, rqUtils, } from '../../utils';
 import { LOGIN, SET_CURRENT_USER, } from './constants';
-
+import { Player, } from 'connect_four_functional';
 const { connRef, fireApp, auth, db, getOnlineRef, } = fireUtils;
 const { rqConstants, rqActions, } = rqUtils;
+const { player, setID, } = Player;
 
 const set = user => () => user;
 const unset = () => () => null;
@@ -15,7 +16,6 @@ const loginSuccess = rqActions(LOGIN).success;
 export const setCurrentUser = u => ({ type: SET_CURRENT_USER, curry: set(u), });
 
 export const setCurrent = u => dispatch =>
-
    Promise.resolve(dispatch(setCurrentUser(u)))
      .then((arg) => {
        console.log('datab', db.ref('online'));
@@ -26,8 +26,12 @@ export const setCurrent = u => dispatch =>
 export const login = dispatch =>
   Promise.resolve(dispatch(loginPending()))
     .then(() => auth.signInAnonymously()
-      .then(user => Promise.all(
-      [ loginSuccess(user), setCurrent(user), ].map(dispatch)))
+      .then(u => setID(u.uid)(u))
+      .then((user) => {
+        console.log('user,', JSON.stringify(user));
+        return Promise.all(
+      [ loginSuccess(user), setCurrent(user), ].map(dispatch));
+      })
       .catch((error) => {
         if (error.code === 'auth/operation-not-allowed') {
           alert('You must enable Anonymous auth in the Firebase Console.');
