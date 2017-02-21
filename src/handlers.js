a1689b477604525a21fc247f27d2849c671178b1
theory friction practice <jackson.brebnor@gmail.com>
2017-02-21 12:04:41 -0500

    adds handlers to modules

--- src/handlers.js
+++ src/handlers.js
@@ -19,16 +19,4 @@ export const onlineHandler = (store) => {
   onlineRef.limitToLast(10).on('child_removed', (snap) => {
     store.dispatch(removeUser(snap.val()));
   });
-
-  // onlineRef.limitToLast(10).on('child_removed', (snap) => {
-    // store.dispatch(addUser(snap.val()));
-  // });
-  // onlineRef.limitToLast(10).on('value', (snap) => {
-  //   // console.log('adding online', u);
-  //
-  //   snap.forEach(c => console.log('cval', c.val()));
-  //   console.log('child_added', snap.val());
-  //
-  //   // return store.dispatch(setUsers(snap.val()));
-  // });
 };
