c05cc369878e7de60b27aa3ef4459a1705c25aae
theory friction practice <jackson.brebnor@gmail.com>
2017-06-06 11:01:59 -0400

    playe div

--- src/components/users.js
+++ src/components/users.js
@@ -3,23 +3,34 @@ import { connect, } from 'react-redux';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
 import { UserActs, } from '../modules';
-
-const mapStateToProps = ({ users, auth: { user, }, }) => ({ users, id: user && user.id, });
-
-const Users = ({ users, addUser, id, }) => (
-      <Grid container direction="column" justify="center" align="center" className="App">
-        <Grid item xs={11} className="App-header">
-          <h1>the current user ID is {id}</h1>
-          <Button onClick={() => addUser('11')}>Add User</Button>
-
-          <p>users</p>
-          <ul>
-            {users.map((u, i) => <p key={i}>user:{JSON.stringify(u)}</p>)}
-          </ul>
-          <p>users</p>
-
-        </Grid>
-      </Grid>
-    );
+import { Game, } from 'connect_four_functional';
+import Text from 'material-ui/Typography';
+import List, { ListItem, ListItemAvatar, ListItemIcon, ListItemText, ListSubheader, } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+const mapStateToProps = ({ users, game, auth: { user, }, }) =>
+({ users, game, id: user && user.id, active: Game.active(game), });
+const Users = ({ users, addUser, id, game, active, }) => (
+     <Grid container direction="column" justify="center" align="center" className="App">
+       <Grid item xs={11} className="App-header">
+         {active && <Text align="center">
+           {`the current player is ${active.name || active.id}`}
+         </Text>}
+         <List dense>
+           <ListSubheader>
+             <Text align="center">users</Text>
+           </ListSubheader>
+           {game.players.map((u, i) => (
+             <ListItem key={u.id}>
+               <ListItemAvatar>
+                 <Avatar>
+                   {`${i}`}
+                 </Avatar>
+               </ListItemAvatar>
+               <ListItemText primary={u.name || u.id} secondary={ u.id} />
+             </ListItem>))}
+         </List>
+       </Grid>
+     </Grid>
+   );
 
 export default connect(mapStateToProps, UserActs)(Users);
