97ae232d739a41cd8575f1d5b66da74c41e20654
theory friction practice <jackson.brebnor@gmail.com>
2017-08-18 17:30:27 -0400

    playesr and board

--- src/components/game/main.js
+++ src/components/game/main.js
@@ -3,31 +3,55 @@ import Grid from 'material-ui/Grid';
 import { connect } from 'react-redux';
 import { Game } from 'connect_four_functional';
 import Button from 'material-ui/Button';
-import Card, { CardActions, CardContent, CardHeader } from 'material-ui/Card';
+import Card, {
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+} from 'material-ui/Card';
+
+// import React from 'react';
+// import Grid from 'material-ui/Grid';
+// import { Game, } from 'connect_four_functional';
+// import { connect, } from 'react-redux';
+import { PlayerCard } from '../player';
+import { pSort } from '../../utils/viz';
+
+// const stateToProps = ({ game, }) => ({ players: pSort(Game.players(game)), });
+
+// const Players = ({ players, }) => (
+//   <Grid container align="center" justify="center">
+//     {players.map((p, i) => (
+//       <Grid item xs sm={6} key={p.id}>
+//         <PlayerCard player={p}/>
+//       </Grid>
+//     ))}
+//   </Grid>
 
 import { GameActs } from '../../modules';
 import Alert from './alert';
 import Board from './board';
 import Players from './players';
 
-const { winner } = Game;
+const { winner, players: getPlrs } = Game;
 const isOver = game => game.players.length > 1 && winner(game);
 
-const stateToProps = ({ game }) => ({ game, ended: isOver(game) });
+const stateToProps = ({ game }) => ({
+  game,
+  ended: isOver(game),
+  players: pSort(getPlrs(game)),
+});
 
 const GameComponent = ({ start, ended, game, resetGame, clearGame }) =>
   (<Grid container align="center" justify="center">
     {ended && <Alert open={ended} />}
-    <Grid item xs={11} className="Alert">
+    <Grid item xs={9} className="Alert">
       <Board />
     </Grid>
 
     <Grid item xs={11} className="GameGrid">
       <Card>
-        <CardHeader title={`in play?${game.inPlay}`} />
-        <CardContent>
-          <Players />
-        </CardContent>
+        <CardHeader title={<Players />} />
         <CardActions>
           <Button onClick={start}>Start game</Button>
           <Button onClick={clearGame}>clearGame game</Button>
