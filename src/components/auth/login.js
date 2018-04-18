187424964faceccd047f6111d19888f0e9672828
tfpractice <jackson.brebnor@gmail.com>
2018-04-17 21:51:55 -0400

    linting

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Button from 'material-ui/Button';
-import { connect } from 'react-redux';
-import { Field } from 'redux-form';
+import Button from "material-ui/Button";
+import React from "react";
+import { connect } from "react-redux";
+import { Field } from "redux-form";
 
-import { AuthActs } from '../../modules';
-import { ClearForm, renderText } from '../../utils';
-import LogoutLink from './logoutLink';
+import LogoutLink from "./logoutLink";
+import { AuthActs } from "../../modules";
+import { ClearForm, renderText } from "../../utils";
 
-const baseLogin = ({ handleSubmit }) =>
-  (<form onSubmit={handleSubmit}>
+const baseLogin = ({ handleSubmit }) => (
+  <form onSubmit={handleSubmit}>
     <Field
       name="displayName"
       component={renderText}
@@ -16,10 +16,13 @@ const baseLogin = ({ handleSubmit }) =>
     />
     <Button type="submit">Login</Button>
     <LogoutLink />
-  </form>);
+  </form>
+);
+
 const ReduxLogin = ClearForm(baseLogin);
 
-const LoginForm = ({ login, formID }) =>
-  <ReduxLogin form={formID} onSubmit={login} />;
+const LoginForm = ({ login, formID }) => (
+  <ReduxLogin form={formID} onSubmit={login} />
+);
 
 export default connect(null, AuthActs)(LoginForm);
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
-
 import { combineReducers } from "redux";
 import { reducer as form } from "redux-form";
+
 import { auth, game, users } from "./modules";
 
 export default combineReducers({ users, form, auth, game });
